86f711b046e904101d68bd0c936c7d4fdd627f52
ValentinIliev5 <justwannaball47@gmail.com>
2024-03-27 12:08:35 +0200

    add filtering for queue types

--- JSONTransformer/main.py
+++ JSONTransformer/main.py
@@ -4,14 +4,35 @@ import ujson
 dataF = open('matchesData.json')
 
 data = ujson.load(dataF)
-
-dictionary = data[0]
-info = dictionary["info"]
-participants = info["participants"]
-
-game_duration = info["gameDuration"]
-queue_id = info["queueId"]
+dataF.close()
 
 
-for value in participants[0]:
-    print(value)
+good_queues = [400, 420, 450]
+
+filtered_data = [element for element in data
+                 if element is not None
+                 and element["info"]["queueId"] in good_queues]
+
+faults = 0
+
+for element in filtered_data:
+    try:
+        print(element["info"]["queueId"])
+    except TypeError:
+        faults = faults + 1
+
+print("faults: ", faults)
+
+print("old data size, new data size: ", len(data), len(filtered_data))
+
+
+# dictionary = data[7]
+# info = dictionary["info"]
+# participants = info["participants"]
+
+# game_duration = info["gameDuration"]
+# queue_id = info["queueId"]
+
+# print(participants[5]["lane"])
+# for value in participants[0]:
+#    print(value)
